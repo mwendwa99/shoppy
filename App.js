b43336a072e6ff809513e511f9af3ace481c1036
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2021-09-17 18:33:09 +0300

    add react-native-community/hooks

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Alert, View, StatusBar, SafeAreaView, Button, Platform } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
 
 export default function App() {
+
+  const { landscape } = useDeviceOrientation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{
+        backgroundColor: 'black',
+        width: '100%',
+        height: landscape ? '100%' : '30%'
+      }} >
+
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
